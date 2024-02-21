48d15b84084c676d1d3ca08ad10db39e9cc929e1
shashibhushanvirajaji <shashi.virajaji@gmail.com>
2024-02-21 20:01:02 +0530

    added a new test to spec.cy

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('title check',()=>{
+    cy.visit('https://example.cypress.io')
+    cy.url().contains("Cypress.io: Kitchen Sink")
+  })
 })
